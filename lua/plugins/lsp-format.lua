46a43106b7523cf316bb67ca13eb9dcb0ea5e13d
26huitailang <26huitailang@gmail.com>
2023-07-25 14:26:14 +0800

    update lsp-format efm-langserver

---
+++ lua/plugins/lsp-format.lua
@@ -0,0 +1,42 @@
+return {
+    "lukas-reineke/lsp-format.nvim",
+    dependencies = {
+        "mattn/efm-langserver",
+        "creativenull/efmls-configs-nvim",
+    },
+    config = function()
+        require("lsp-format").setup({
+            -- typescript = {
+            --     tab_width = function()
+            --         return vim.opt.shiftwidth:get()
+            --     end,
+            -- },
+            -- yaml = { tab_width = 2 },
+        })
+        local prettier = {
+            formatCommand = [[prettier --stdin-filepath ${INPUT} ${--tab-width:tab_width}]],
+            formatStdin = true,
+        }
+        local black = {
+            formatCommand = "black --quiet -",
+            formatStdin = true,
+        }
+        require "lspconfig".efm.setup {
+            init_options = { documentFormatting = true },
+            settings = {
+                rootMarkers = { ".git/" },
+                languages = {
+                    lua = {
+                        { formatCommand = "lua-format -i", formatStdin = true }
+                    },
+                    typescript = { prettier },
+                    yaml = { prettier },
+                    python = {
+                        require('efmls-configs.linters.flake8'),
+                        require('efmls-configs.formatters.black'),
+                    },
+                }
+            }
+        }
+    end
+}
