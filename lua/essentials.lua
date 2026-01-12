631b4a64b63de7622b628cedebdbb56c2e283566
26huitailang <26huitailang@gmail.com>
2026-01-12 14:13:16 +0800

    feat(markdown): set tab options for markdown files
    Add autocmd to configure tabstop, shiftwidth, softtabstop and expandtab for markdown files to improve editing experience

--- lua/essentials.lua
+++ lua/essentials.lua
@@ -63,3 +63,13 @@ vim.cmd([[
     autocmd InsertLeave * lua require('imselect').setInputMethod()
   augroup END
 ]])
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+})
